5231b14b372686c4d99813ecfb2c5b14068ff630
Robin Nowak <nowak.robin33@gmail.com>
2021-01-23 01:00:19 +0100

    chore(refactor): Extracted the handleFarewell function into a dedicated service.

--- src/Services/CommandService.js
+++ src/Services/CommandService.js
@@ -1,12 +1,10 @@
-const Discord = require('discord.js')
-const _ = require('lodash')
+const FarewellService = require('../Services/FarewellService.js')
 
-const farewells = require('../assets/farewells.json')
 
 commands = {
     "!ping": (message) => message.channel.send('Pong.'),
     "!gg": (message) => message.channel.send('ez u suckers'),
-    "!bye": (message) => message.channel.send(handleFarewell(message)),
+    "!bye": (message) => message.channel.send(FarewellService(message)),
     "!rushb": (message) => message.channel.send("rush B blyat       no stop idiot!"),
     "!1337": (message) => message.channel.send("<a:partydinosaur:725999098930659329>")
 }
@@ -21,18 +19,6 @@ function handleMessage(message) {
     }
 }
 
-// TODO @Username       -- An Client.Users hängen alle User die auf dem Discord angemeldet sind mit username und Id --> ID wird zum @'en benötigt
-// Vielleicht starts with (<) und ends with (>)
-// handleFarewell auslagern in farewellService.js
-function handleFarewell(message) {
-    const userToFarewell = message.content.substr(5, message.content.length)
-    const randomNumber = _.random(0, farewells.length - 1)
-    const farewell = farewells[randomNumber]
-    const farewellWithName = farewell + ' ' + userToFarewell
-
-    console.log(userToFarewell)
-    return farewellWithName
-}
 
 module.exports =  {
     handleMessage
---
+++ src/Services/FarewellService.js
@@ -0,0 +1,19 @@
+const _ = require('lodash')
+
+const farewells = require('../assets/farewells.json')
+
+// TODO @Username       -- An Client.Users hängen alle User die auf dem Discord angemeldet sind mit username und Id --> ID wird zum @'en benötigt
+// Vielleicht starts with (<) und ends with (>)
+// handleFarewell auslagern in farewellService.js
+
+function handleFarewell(message) {
+    const userToFarewell = message.content.substr(5, message.content.length)
+    const randomNumber = _.random(0, farewells.length - 1)
+    const farewell = farewells[randomNumber]
+    const farewellWithName = farewell + ' ' + userToFarewell
+
+    console.log(userToFarewell)
+    return farewellWithName
+}
+
+module.exports = handleFarewell
